8f0eb33ac7d44f8fafa3d0d52a22c6c0c3ff6488
Dozy <dotuan14747@gmail.com>
2025-02-20 09:49:46 +0200

    Created schema for AffiliateStat in models/AffiliateStat.js. Exported dataAffiliateStat to mongoose database. Created getPerformance aggregate function in controllers/management.js to handle retrieve request for Affiliate sales performance. Created router to get Performance in routes/management.js

--- ad_server/index.js
+++ ad_server/index.js
@@ -16,7 +16,8 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
+import AffiliateStat from './models/AffiliateStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from './data/index.js';
 
 /* CONFIGURATION */
 dotenv.config();
@@ -48,4 +49,5 @@ mongoose
         // ProductStat.insertMany(dataProductStat);
         // Transaction.insertMany(dataTransaction);
         // OverallStat.insertMany(dataOverallStat);
+        // AffiliateStat.insertMany(dataAffiliateStat);
     }).catch((error) => console.log(`${error} did not connect`));
